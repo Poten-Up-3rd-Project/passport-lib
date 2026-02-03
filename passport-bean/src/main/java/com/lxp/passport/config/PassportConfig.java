3f93a45966273bdfcdbc46bde20dc3f035ebb888
dnya0 <annay01794@gmail.com>
2026-02-03 12:17:59 +0900

    fix: register PassportVerifier to satisfy PassportFilter dependency

--- passport-bean/src/main/java/com/lxp/passport/config/PassportConfig.java
+++ passport-bean/src/main/java/com/lxp/passport/config/PassportConfig.java
@@ -1,5 +1,7 @@
 package com.lxp.passport.config;
 
+import com.lxp.passport.support.PassportEncoder;
+import com.lxp.passport.support.PassportVerifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +15,18 @@ public class PassportConfig {
     @ConditionalOnMissingBean
     public SecretKey passportSecretKey(KeyProperties properties) {
         return properties.toSecretKey();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public PassportVerifier passportVerifier(KeyProperties properties) {
+        return new PassportVerifier(properties);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public PassportEncoder passportEncoder(KeyProperties properties) {
+        return new PassportEncoder(properties);
     }
 
 }
