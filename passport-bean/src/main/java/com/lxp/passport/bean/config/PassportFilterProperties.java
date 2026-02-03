e62813dc4b42312430b28fcb0b0e779890a3b2ea
dnya0 <annay01794@gmail.com>
2026-02-03 17:37:49 +0900

    fix: fix NPE in PassportFilterProperties by switching to setter-based binding

--- passport-bean/src/main/java/com/lxp/passport/bean/config/PassportFilterProperties.java
+++ passport-bean/src/main/java/com/lxp/passport/bean/config/PassportFilterProperties.java
@@ -1,20 +1,16 @@
 package com.lxp.passport.bean.config;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-import java.util.Collections;
 import java.util.List;
 
-import static java.util.Objects.isNull;
-
+@Setter
 @Getter
 @ConfigurationProperties(prefix = "passport.filter")
 public class PassportFilterProperties {
 
-    private final List<String> excludePaths;
+    private List<String> excludePaths;
 
-    public PassportFilterProperties(List<String> excludePaths) {
-        this.excludePaths = isNull(excludePaths) ? Collections.emptyList() : excludePaths;
-    }
 }
