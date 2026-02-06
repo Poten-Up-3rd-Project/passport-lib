ec0382412aa2a451daa5f7e8a71f74721a9c0958
dnya0 <annay01794@gmail.com>
2026-02-06 18:10:02 +0900

    refactor: make RequireRole.value optional with default

--- passport-authorization/src/main/java/com/lxp/passport/authorization/annotation/RequireRole.java
+++ passport-authorization/src/main/java/com/lxp/passport/authorization/annotation/RequireRole.java
@@ -9,7 +9,7 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface RequireRole {
 
-    String[] value();
+    String[] value() default {};
 
     String[] anyOf() default {};
 
